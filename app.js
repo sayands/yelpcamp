7dd939a8df0a1d480cb609054a2287507f73db67
sayands <sayandsarkar.1997@gmail.com>
2018-05-20 19:32:37 +0300

    Created Campgrounds

--- app.js
+++ app.js
@@ -1,29 +1,46 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
 
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
+
+var campgrounds = [
+  {
+    name: "Darjeeling",
+    image: "http://outgotrip.com/wp-content/uploads/2017/08/DARJEELING.jpg"
+  },
+  {
+    name: "Darjeeling",
+    image: "http://outgotrip.com/wp-content/uploads/2017/08/DARJEELING.jpg"
+  },
+  {
+    name: "Darjeeling",
+    image: "http://outgotrip.com/wp-content/uploads/2017/08/DARJEELING.jpg"
+  }
+];
 
 app.get("/", function(req, res) {
   res.render("landing");
 });
 
 app.get("/campgrounds", function(req, res) {
-  var campgrounds = [
-    {
-      name: "Darjeeling",
-      image: "http://outgotrip.com/wp-content/uploads/2017/08/DARJEELING.jpg"
-    },
-    {
-      name: "Darjeeling",
-      image: "http://outgotrip.com/wp-content/uploads/2017/08/DARJEELING.jpg"
-    },
-    {
-      name: "Darjeeling",
-      image: "http://outgotrip.com/wp-content/uploads/2017/08/DARJEELING.jpg"
-    }
-  ];
-
   res.render("campgrounds", { campgrounds });
+});
+
+app.post("/campgrounds", function(req, res) {
+  // get data from form and add to campgrounds array
+  var { name, image } = req.body;
+
+  var newCampground = { name, image };
+  campgrounds.push(newCampground);
+
+  //redirect back to campgrounds page
+  res.redirect("/campgrounds");
+});
+
+app.get("/campgrounds/new", function(req, res) {
+  res.render("new.ejs");
 });
 
 app.listen(process.env.PORT || 3000, process.env.IP, function() {
