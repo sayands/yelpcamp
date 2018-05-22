a516b009a3966d11078f2e9f2a7aef8b15f56457
sayands <sayandsarkar.1997@gmail.com>
2018-05-22 16:13:58 +0300

    Completed Auth Functionality

--- app.js
+++ app.js
@@ -28,6 +28,11 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
+app.use(function(req, res, next) {
+  res.locals.currentUser = req.user;
+  next();
+});
+
 // =======================
 // APP CONFIGURATION
 // =======================
@@ -52,6 +57,7 @@ app.get("/", function(req, res) {
 
 //INDEX - Show all campgrounds
 app.get("/campgrounds", function(req, res) {
+  //console.log(req.user);
   //Get all campgrounds from DB
   Campground.find({}, function(err, allCampgrounds) {
     if (err) {
@@ -104,7 +110,7 @@ app.get("/campgrounds/:id", function(req, res) {
 // COMMENTS ROUTES
 // =====================================================
 
-app.get("/campgrounds/:id/comments/new", function(req, res) {
+app.get("/campgrounds/:id/comments/new", isLoggedIn, function(req, res) {
   // find campground by Id
   Campground.findById(req.params.id, function(err, campground) {
     if (err) {
@@ -115,7 +121,7 @@ app.get("/campgrounds/:id/comments/new", function(req, res) {
   });
 });
 
-app.post("/campgrounds/:id/comments", function(req, res) {
+app.post("/campgrounds/:id/comments", isLoggedIn, function(req, res) {
   //lookup campground using ID
   Campground.findById(req.params.id, function(err, campground) {
     if (err) {
@@ -139,9 +145,9 @@ app.post("/campgrounds/:id/comments", function(req, res) {
   });
 });
 
-// ==================
-// AUTH ROUTES
-// ==================
+// ===================================
+// REGISTER ROUTES
+// ===================================
 
 // show resgister form
 app.get("/register", function(req, res) {
@@ -161,6 +167,45 @@ app.post("/register", function(req, res) {
     });
   });
 });
+
+// ================================
+// LOGIN ROUTES
+//=================================
+
+// show login form
+app.get("/login", function(req, res) {
+  res.render("login");
+});
+
+//handles login logic
+app.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/campgrounds",
+    failureRedirect: "/login"
+  }),
+  function(req, res) {
+    res.send("LOGIN LOGIC handles here");
+  }
+);
+
+// =================================
+// LOGOUT ROUTES
+// =================================
+app.get("/logout", function(req, res) {
+  req.logout();
+  res.redirect("/campgrounds");
+});
+
+// ============
+// MIDDLEWARE
+// ============
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect("/login");
+}
 
 app.listen(process.env.PORT || 3000, process.env.IP, function() {
   console.log("YELPCAMP server has started");
