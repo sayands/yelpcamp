1a4f410a46b8c16c8e2fb481ebbcc3026debf1ad
sayands <sayandsarkar.1997@gmail.com>
2018-05-22 13:34:07 +0300

    Added User model

--- app.js
+++ app.js
@@ -2,9 +2,12 @@ var express = require("express"),
   app = express(),
   bodyParser = require("body-parser"),
   mongoose = require("mongoose"),
+  passport = require("passport"),
+  LocalStrategy = require("passport-local"),
   Campground = require("./models/campground"),
-  Comment = require("./models/comment");
-seedDB = require("./seeds");
+  Comment = require("./models/comment"),
+  User = require("./models/user"),
+  seedDB = require("./seeds");
 
 //seedDB();
 
