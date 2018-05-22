b6841044cf336abf4ea168f044d34b5db3d45641
sayands <sayandsarkar.1997@gmail.com>
2018-05-22 13:16:51 +0300

    Added Styling To Components

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ mongoose.connect(
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
 
 app.get("/", function(req, res) {
   res.render("landing");
