b42275108fcbf9326c0aedac92b1b553be9657ea
sayands <sayandsarkar.1997@gmail.com>
2018-05-23 11:19:41 +0300

    Added User associations with Campground

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -20,24 +20,28 @@ router.get("/", function(req, res) {
 });
 
 //CREATE - Add new campground to DB
-router.post("/", function(req, res) {
+router.post("/", isLoggedIn, function(req, res) {
   // get data from form and add to campgrounds array
   var { name, image, description } = req.body;
-
-  var newCampground = { name, image, description };
+  var author = {
+    id: req.user._id,
+    username: req.user.username
+  };
+  var newCampground = { name, image, description, author };
   //Create a new campground and save to DB
   Campground.create(newCampground, function(err, newlyCreated) {
     if (err) {
       console.log(err);
     } else {
       //redirect back to campgrounds page
+      console.log(newlyCreated);
       res.redirect("/campgrounds");
     }
   });
 });
 
 //NEW - Show form to create new campgrounds
-router.get("/new", function(req, res) {
+router.get("/new", isLoggedIn, function(req, res) {
   res.render("campgrounds/new");
 });
 
@@ -56,5 +60,13 @@ router.get("/:id", function(req, res) {
       }
     });
 });
+
+//middleware
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect("/login");
+}
 
 module.exports = router;
